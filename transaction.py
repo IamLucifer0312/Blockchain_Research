6adc37c6e2feaeb06128a3a1de14ac32a4349234
tanquang206 <truongtanquang2910@gmail.com>
2025-05-03 18:34:51 +0700

    Update transaction.py

--- transaction.py
+++ transaction.py
@@ -6,3 +6,6 @@ class Transaction:
         self.receiver = receiver  
         self.amount = amount        
         self.timestamp = datetime.now() 
+    
+    def __str__(self):
+        return f'Transaction from {self.sender} to {self.receiver} of {self.amount} at {self.timestamp}'
