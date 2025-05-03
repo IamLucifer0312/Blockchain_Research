51f6a9b5cfe2123e716f23785d6ef0c1c0d547e8
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2025-05-03 10:33:17 +0700

    Add block class

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
-print("Hello World")
-print("AA")
-print("BB")
+from block import Block
+import time
+
+if __name__ == "__main__":
+    # Create a new block
+    block = Block(0, "0", time.time(), "Block data")
+    
+    # Mine the block with a difficulty of 4
+    block.mine_block(4)
+    
+    # Print the block's hash
+    print(f"Block hash: {block.hash}")
